6c134e6f563aacb6c98eccf3b523ad33dd6abede
PrAndrian <princydruh@gmail.com>
2022-11-08 22:50:53 +0100

    add error check condition

--- starterOnly/modalForm.js
+++ starterOnly/modalForm.js
@@ -1,11 +1,3 @@
-const checkField = document.querySelectorAll(".checkbox-input");
-const checkError = document.querySelector("#checkbox-input-error");
-
-const checkFieldCondition = document.querySelector("#checkbox1");
-const checkFieldConditionError = document.querySelector("#checkFieldConditionError");
-
-checkError.style.display="none";
-checkFieldConditionError.style.display="none";
 
 //--Conditions
 
@@ -14,6 +6,9 @@ function conditions(field){
     const lastField = document.querySelector("#last");
     const emailField = document.querySelector("#email");
     const quantityField = document.querySelector("#quantity");
+    const birthdateField = document.querySelector("#birthdate");
+    // const radioField = document.querySelectorAll(".checkbox-input");
+    const checkFieldCondition = document.querySelector("#checkbox1");
    
     if(field==firstField || field==lastField){
         return (field.value.length == 0 || field.value.length < 2);
@@ -26,6 +21,26 @@ function conditions(field){
     if(field==quantityField){
         return (Number.isInteger(field.value) || field.value == 0 || field.value < 0 );
     }
+
+    if(field==birthdateField){
+        return (field.value.length==0);
+    }
+  
+    if(field==checkFieldCondition){
+        return (field.checked == false);
+    }
+
+    // let checkArray = Array();
+    // for( i=1; i<7; i++){
+    //     result = document.getElementById('location'+i).checked;
+    //     checkArray.push(result);
+    // }
+
+    // if(field==radioField){
+    //     return checkArray.includes(true);
+    // }     
+    
+
 }
 
 //----checkInput
@@ -38,6 +53,17 @@ function checkInput(field,onsubmit,handler){
         field.parentElement.setAttribute("data-error-visible", "false");
     }
 }
+
+// function checkInput_cities(field,onsubmit,handler){
+//     if(conditions(field)){
+//         onsubmit.preventDefault();
+//         field[0].parentElement.setAttribute("data-error-visible", "true");
+//         handler(field);
+//     }else{
+//         field[0].parentElement.setAttribute("data-error-visible", "false");
+//     }
+// }
+
 //----handler
 function handleChange_first_last(field){
     field.addEventListener("input", (event)=>{
@@ -56,6 +82,30 @@ function handleChange_quatity(field){
         error_message_quatity(field,event);
     });
 }
+
+function handleChange_birthdate(field){
+    field.addEventListener("input", (event)=>{
+        error_message_birthdate(field,event);
+    });
+}
+
+function handleChange_checkFieldCondition(field){
+    field.addEventListener("input", (event)=>{
+        error_message_checkFieldCondition(field,event);
+    });
+}
+
+// function handleChange_radio(field){
+//     let checkArray = Array();
+//     for( i=1; i<7; i++){
+//         result = document.getElementById('location'+i).checked;
+//         checkArray.push(result);
+//     }
+    
+//     field.addEventListener("input", (event)=>{
+//         error_message_radio(field,event,checkArray);
+//     });
+// }
 
 //----error-message
 function error_message_first_last(field,event){
@@ -82,25 +132,47 @@ function error_message_quatity(field,event){
     }
 }    
 
+function error_message_birthdate(field,event){
+    if(event.target.value.length==0){
+        field.parentElement.setAttribute("data-error-visible", "true");
+    }else{
+        field.parentElement.setAttribute("data-error-visible", "false");
+    }
+}    
+
+function error_message_checkFieldCondition(field,event){
+    if(event.target.checked == false){
+        field.parentElement.setAttribute("data-error-visible", "true");
+    }else{
+        field.parentElement.setAttribute("data-error-visible", "false");
+    }
+}    
+
+// function error_message_radio(field,event,radioArray){
+//     if(radioArray.includes(true)){
+//         field.parentElement.setAttribute("data-error-visible", "false");
+//     }else{
+//         field.parentElement.setAttribute("data-error-visible", "true");
+//     }
+// }    
+
     
 document.querySelector(".btn-submit").addEventListener("click", (event)=>{
     const firstField = document.querySelector("#first");
     const lastField = document.querySelector("#last");
     const emailField = document.querySelector("#email");
     const quantityField = document.querySelector("#quantity");
+    const birthdateField = document.querySelector("#birthdate");
+    // const radioField = document.querySelectorAll(".checkbox-input");
+    const checkFieldCondition = document.querySelector("#checkbox1");
 
     checkInput(firstField,event,handleChange_first_last);
     checkInput(lastField,event,handleChange_first_last);
     checkInput(emailField,event,handleChange_email);
     checkInput(quantityField,event,handleChange_quatity);
-
-    // if((Number.isInteger(quantityField.value) || quantityField.value <= 0)){
-    //     error_message(quantityError);
-    //     event.preventDefault();
-    // }else{
-    //     quantityError.style.display="none";
-    //     event.preventDefault();
-    // }
+    checkInput(birthdateField,event,handleChange_birthdate);
+    checkInput(checkFieldCondition,event,handleChange_checkFieldCondition);
+    // checkInput_cities(radioField,event,handleChange_radio);
 
     // let checkArray = Array();
     // for( i=1; i<7; i++){
